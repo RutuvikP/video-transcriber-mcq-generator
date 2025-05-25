68dc0b0f5efd47bf61d69ab25009024c80266e28
Rutuvik Patil <you@example.com>
2025-05-25 14:10:05 +0530

    feat: generate mcq

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from whisper_service import transcribe_video
 import uvicorn
+from pydantic import BaseModel
+from mcq_generator import generate_mcqs_from_text
 
 app = FastAPI()
+
+class Segment(BaseModel):
+    text: str
 
 @app.post("/transcribe")
 async def transcribe(file: UploadFile = File(...),fileName: str = Form(None)):
     transcription = await transcribe_video(file,fileName)
     return {"transcript": transcription}
+
+@app.post("/generate-mcq")
+async def generate_mcq(segment: Segment):
+    mcq = generate_mcqs_from_text(segment.text)
+    return {"mcq": mcq}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
